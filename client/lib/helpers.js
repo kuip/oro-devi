13e185d89015f4bd3c22ad43a8e5f173d4cbdcb3
Loredana Cirstea <loredana.cirstea@gmail.com>
2016-12-06 21:36:39 +0200

    getPage, pathParams

--- client/lib/helpers.js
+++ client/lib/helpers.js
@@ -1,6 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { getPage } from '/client/lib/vars';
+
 
 Template.registerHelper('log', function(a) {
 	console.log(a);
+});
+
+Template.registerHelper('getPage', () => {
+	return getPage.get();
 });
