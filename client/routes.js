3ac28b5e0a35d01aa454460ea90f5932c51c4730
Loredana Cirstea <loredana.cirstea@gmail.com>
2016-12-16 20:54:30 +0200

    Set custom routes from the db.

--- client/routes.js
+++ client/routes.js
@@ -1,57 +1,59 @@
 import { Meteor } from 'meteor/meteor';
+import { ReactiveVar } from 'meteor/reactive-var';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
 
-let customRoutes = ORO.globalCustomRoutes;
+let router, routes,
+  customRoutes = new ReactiveVar(),
+  customRouter = new ReactiveVar();
 
-var routes = React.createElement(
+routes = React.createElement(
   Route,
   { path: "/", component: AppBody },
   React.createElement(IndexRoute, { component: AppLoading }),
   React.createElement(Route, { path: "files(/**)", component: FilesComponent }),
   React.createElement(Route, { path: "file/**", component: FileComponent }),
   React.createElement(Route, { path: "svg", component: SvgComponent }),
-  React.createElement(Route, { path: "app/**", component: AppComponent }),
-
-  //React.createElement(Route, { path: "*", component: AppNotFound }),
+  React.createElement(Route, { path: "app/**", component: AppComponent })
 );
 
+router = React.createElement(
+  Router,
+  { 
+    history: browserHistory,
+    onUpdate: function() {
+      customRouter.set(this);
+    }
+  },
+  routes
+);
 
 
 // Customized routes from the db
 Meteor.subscribe('routes');
 
-// Temporary (replace with server cache)
-customRoutesRec = {"script":"{\n    \"/\": \"app/neurals-v1/home\"\n}"};
-console.log(customRoutesRec.script)
-customRoutes = JSON.parse(customRoutesRec.script);
-
-/*Tracker.autorun(() => {
-  customRoutes = OroFile.find({title: {$regex: /devicore\/routes/ }}).fetch();
-  console.log(JSON.stringify(customRoutes));
-});*/
+Tracker.autorun(() => {
+  file = OroFile.findOne({title: {$regex: /devicore\/routes/ }});
+  if(file && file.script) {
+    customRoutes.set(JSON.parse(file.script));
+  }
+});
 
 
 Meteor.startup(function () {
-  /*let rr = Meteor.call('getRoutes', (err, res) => {
-    console.log(res);
-  });*/
-  
-  var router = React.createElement(
-  Router,
-  { 
-    history: browserHistory,
-    onUpdate: function() {
-      //console.log(this);
-      let redir = customRoutes[this.router.location.pathname];
-      if(redir) {
-        console.log('redirect to: ' + redir);
-        this.props.history.push(redir);
-      }
-    }
-  },
-  routes
-);
   ReactDOM.render(router, document.getElementById("app-container"));
+
+  Tracker.autorun(function() {
+    let rr = customRoutes.get();
+    let r = customRouter.get();
+
+    if(rr && r) {
+      let redir = rr[r.router.location.pathname];
+        if(redir) {
+          console.log('redirect to: ' + redir);
+          r.props.history.push(redir);
+        }
+    }
+  })
 });
