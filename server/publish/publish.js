6aaad829ae6028b7f2694e15e3c546443a515d44
Loredana Cirstea <loredana.cirstea@gmail.com>
2017-01-20 13:58:36 -0700

    Fix sorting for publishing files

--- server/publish/publish.js
+++ server/publish/publish.js
@@ -15,7 +15,9 @@ Meteor.publish('files', function(query, options = {}) {
 
   if(!query)
     return null;
-  options.dateModified = -1;
+
+  options.sort = options.sort || {};
+  options.sort.dateCreated = 1;
   return OroFile.find(query, options);
 })
 
